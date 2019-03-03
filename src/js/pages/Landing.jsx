af1373b8b4d49d2d0365fc4f6e6902257cada018
Daria <me.darilla@gmail.com>
2019-03-03 11:29:09 +0100

    apply changes after CR

--- src/js/pages/Landing.jsx
+++ src/js/pages/Landing.jsx
@@ -10,12 +10,14 @@ class LandingPage extends Component {
       seo_title: ""
     };
   }
-  
+
   handleChange = (event) => {
     this.setState({ [event.target.id]: event.target.value });
   }
+
   render() {
     const { seo_title } = this.state;
+
     return (
       <div>
         <Input
@@ -31,7 +33,7 @@ class LandingPage extends Component {
   }
 }
 
+export default LandingPage;
+
 const root = document.getElementById("root");
 root ? ReactDOM.render(<LandingPage />, root) : false;
-
-export default LandingPage;

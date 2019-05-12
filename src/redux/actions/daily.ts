771f5600bf175bf2a97385d0a57a35c3f4f94079
Daria <me.darilla@gmail.com>
2019-05-12 15:04:30 +0200

    apply changes after CR

--- src/redux/actions/daily.ts
+++ src/redux/actions/daily.ts
@@ -1,20 +1,10 @@
 export const CHECK_DAILY = "CHECK_DAILY";
-export const EDIT_FOOD = "EDIT_FOOD";
 
 interface checkDaily {
   type: typeof CHECK_DAILY
   content: string
 }
 
-interface editFood {
-  type: typeof EDIT_FOOD
-  content: any
-}
-
 export function checkDaily(message: string):checkDaily {
   return { type: CHECK_DAILY, content: message };
-}
-
-export function editFood(food: any):editFood {
-  return { type: EDIT_FOOD, content: food };
 }
